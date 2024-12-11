0bb640efebd872163e933ba0cbd70c4ec14e38d6
kraoshin <killian.kerleau@outlook.fr>
2024-12-11 13:07:49 +0100

    Write a function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort a doubly linked list using insertion sort
+ * @list: list to sort
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *tmp;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+
+	while (current)
+	{
+		tmp = current;
+
+		while (tmp->prev && tmp->n < tmp->prev->n)
+		{
+			tmp->prev->next = tmp->next;
+
+			if (tmp->next)
+				tmp->next->prev = tmp->prev;
+
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+
+			if (tmp->prev)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+
+			tmp->next->prev = tmp;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
